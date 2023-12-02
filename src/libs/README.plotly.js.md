2afbfbff3d5fdd1c6348e6b5639e9900d139d659
lilliputten <lilliputten@yandex.ru>
2023-12-02 22:30:47 +0700

    Issue #12: Plotly library included into the project as an option (it's possible to use other options, see `src/libs/plotly/core/PlotlyLib.ts`).

---
+++ src/libs/README.plotly.js.md
@@ -0,0 +1,14 @@
+# plotly.js
+
+Embedded library location:
+
+- `src/libs/plotly.js/`
+
+Default installed library location:
+
+- `node_modules/plotly.js/`
+
+# TODO
+
+- Optimize included modules in `src/libs/plotly.js/lib/index*.js`.
+- Add dependencies from `src/libs/plotly.js/package.json`.
--- src/libs/plotly/core/PlotlyLib.ts
+++ src/libs/plotly/core/PlotlyLib.ts
@@ -1,9 +1,16 @@
-// NOTE: Use dev (patched) version of plotly (required core nodejs polyfills for webpack 5+, see solution in `craco.config.js`)
-import PlotlyLib from 'plotly.js/lib';
-export * from 'plotly.js/lib';
+/* // NOTE: Use dev (patched) version of plotly (required core nodejs polyfills for webpack 5+, see solution in `craco.config.js`)
+ * import PlotlyLib from 'plotly.js/lib';
+ * // export * from 'plotly.js/lib';
+ */
+/* // NOTE: Use production version of plotly
+ * import PlotlyLib from 'plotly.js';
+ * export * from 'plotly.js';
+ */
 
-// // NOTE: Use production version of plotly
-// import PlotlyLib from 'plotly.js';
-// export * from 'plotly.js';
+// NOTE: Use embedded plotly library
+import PlotlyLib from 'src/libs/plotly.js';
+
+// @see [typescript - How to reexport `*` from a module that uses `export =` - Stack Overflow](https://stackoverflow.com/questions/41892470/how-to-reexport-from-a-module-that-uses-export)
+// @see [Re-exporting CJS module with `export =` from ESM via `export *` causes type error · Issue #51923 · microsoft/TypeScript](https://github.com/microsoft/TypeScript/issues/51923)
 
 export default PlotlyLib;
