3972ce4d7a467eac92b77976d57c9a65c7cb4921
lilliputten <lilliputten@yandex.ru>
2023-12-13 18:02:06 +0700

    Issue #9: Added control to manually hide particular nodes.

---
+++ src/components/SankeyMisc/SankeyPropertiesPanel/HideNodeButton.tsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import { observer } from 'mobx-react-lite';
+import { runInAction } from 'mobx';
+import { FormControl, FormHelperText, FormLabel, Switch } from '@mui/material';
+import { MuiColorInput } from 'mui-color-input';
+import classNames from 'classnames';
+
+import { TColor, TPropsWithClassName } from 'src/core/types';
+import { TGraphId } from 'src/core/types/SankeyApp';
+import { useGraphColor } from 'src/hooks/Sankey';
+import { useSankeyAppDataStore } from 'src/components/SankeyApp/SankeyAppDataStore';
+
+interface THideNodeButtonProps extends TPropsWithClassName {
+  graphId: TGraphId | undefined;
+}
+
+export const HideNodeButton: React.FC<THideNodeButtonProps> = observer((props) => {
+  const { className, graphId } = props;
+
+  const sankeyAppDataStore = useSankeyAppDataStore();
+  const { userHiddenGraphNodes } = sankeyAppDataStore;
+
+  if (graphId == null) {
+    return null;
+  }
+
+  const isNodeHidden = graphId != null && userHiddenGraphNodes.includes(graphId);
+
+  const handleHideNodeButton: React.ChangeEventHandler<HTMLInputElement> = (ev) => {
+    const hideNode = ev.target.checked;
+    let updatedList: TGraphId[] | undefined = undefined;
+    if (hideNode) {
+      if (!isNodeHidden) {
+        // Add current id...
+        updatedList = userHiddenGraphNodes.concat(graphId);
+      }
+    } else {
+      if (isNodeHidden) {
+        // Remove current id...
+        updatedList = userHiddenGraphNodes.filter((id) => id !== graphId);
+      }
+    }
+    console.log('[HideNodeButton:handleHideNodeButton]', {
+      isNodeHidden,
+      hideNode,
+      updatedList,
+      userHiddenGraphNodes,
+    });
+    runInAction(() => {
+      if (updatedList) {
+        sankeyAppDataStore.userHiddenGraphNodes = updatedList;
+      }
+    });
+  };
+
+  return (
+    <FormControl className={classNames(className, 'HideNodeButton')}>
+      <FormLabel htmlFor="hideNodeButton">Hide node</FormLabel>
+      <Switch
+        id="hideNodeButton"
+        name="hideNodeButton"
+        aria-labelledby="hideNodeButton"
+        aria-describedby="hideNodeButtonText"
+        checked={isNodeHidden}
+        onChange={handleHideNodeButton}
+        inputProps={{ 'aria-label': 'controlled' }}
+      />
+      <FormHelperText id="hideNodeButtonText">
+        {/* Helper text */}
+        Hide this specific node from the chart.
+      </FormHelperText>
+    </FormControl>
+  );
+});
--- src/helpers/basic/arrays.ts
+++ src/helpers/basic/arrays.ts
@@ -7,3 +7,21 @@ export function cleanupList<T = unknown>(list?: T[]): T[] | undefined {
 export function onlyUnique<T extends unknown>(value: T, index: number, array: T[]) {
   return array.indexOf(value) === index;
 }
+
+export function areTwoSortedArraysEqual<T>(a?: T[], b?: T[]): boolean {
+  if (a == null && b == null) {
+    return true;
+  }
+  if (a == null || b == null) {
+    return false;
+  }
+  if (a.length !== b.length) {
+    return false;
+  }
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) {
+      return false;
+    }
+  }
+  return true;
+}
