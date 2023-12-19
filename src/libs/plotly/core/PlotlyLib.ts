ed140e1def5b44b5616514407b0998c7a4a0edcc
lilliputten <lilliputten@yandex.ru>
2023-12-19 14:59:32 +0700

    Issue #24: Added plotly requirements -- they're stored in the `devDependencies` section of `package.json` (original dependecies moved into `unusedPlotlyDependencies` section). Removed unused plotly library components (from `src/libs/plotly.js/lib/index.js`), updated library imports.

--- src/libs/plotly/core/PlotlyLib.ts
+++ src/libs/plotly/core/PlotlyLib.ts
@@ -8,7 +8,7 @@
  */
 
 // NOTE: Use embedded plotly library
-import PlotlyLib from 'src/libs/plotly.js';
+import PlotlyLib from 'src/libs/plotly.js/lib';
 
 // @see [typescript - How to reexport `*` from a module that uses `export =` - Stack Overflow](https://stackoverflow.com/questions/41892470/how-to-reexport-from-a-module-that-uses-export)
 // @see [Re-exporting CJS module with `export =` from ESM via `export *` causes type error · Issue #51923 · microsoft/TypeScript](https://github.com/microsoft/TypeScript/issues/51923)
