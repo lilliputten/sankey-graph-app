14f05627f465d93aaa33a2427fe7287cf8406c76
lilliputten <lilliputten@yandex.ru>
2024-02-02 22:39:16 +0700

    Build publish.0.0.24, 2024.02.02, 22:35 GMT+7 (240202-2235)

--- start-app.py
+++ start-app.py
@@ -1,5 +1,6 @@
 # -*- coding:utf-8 -*-
-# @changed 2023.12.16, 21:57
+# @since 2024.02.01, 20:08
+# @changed 2024.02.02, 22:08
 
 import os
 from os import path
@@ -13,10 +14,19 @@ import threading
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from time import sleep
 import webbrowser
+import traceback
+import re
+import json
+
+# NOTE: We've got a warning here:
+# DeprecationWarning: 'cgi' is deprecated and slated for removal in Python 3.13
+import cgi
 
 # Constants...
 browserUrlPrefix = 'http://localhost:'
 devBuildFolder = 'build'
+
+acceptPostDataUrl = '/cgi-bin/accept-post-data'
 
 # Default options...
 defaultDataFolder: str = 'data'
@@ -26,13 +36,23 @@ defaultWebPort: str = 8080
 
 
 # Command line options...
-parser = argparse.ArgumentParser(description='Launch app from python script demo.')
-parser.add_argument('--data-folder', dest='dataFolder', metavar='{dataFolder}', action='store', default=defaultDataFolder, help='Data folder name (default: "' + defaultDataFolder + '")')
-parser.add_argument('--data-set-folder', dest='dataSetFolder', metavar='{dataSetFolder}', action='store', default=defaultDataSetFolder, help='Data set folder name (default: "' + defaultDataSetFolder + '")')
-parser.add_argument('--target-folder', dest='targetFolder', metavar='{targetFolder}', action='store', default=defaultTargetFolder, help='Target folder name (default: "' + defaultTargetFolder + '")')
-parser.add_argument('--omit-date-tag', dest='omitDateTag', action=argparse.BooleanOptionalAction, help='Omit date tag postfix for auto-generated target folder name (datetime module required)')
+parser = argparse.ArgumentParser(description='Launch web server for the app.')
+
 parser.add_argument('--web-port', dest='webPort', metavar='{webPort}', type=int, action='store', default=defaultWebPort, help='Web server port (default: "' + str(defaultWebPort) + '")')
+
+#  parser.add_argument('--demo', dest='demo', action=argparse.BooleanOptionalAction, help='Do demo: write demo files and do one of the demo actions: demo-post or demo-files')
+parser.add_argument('--demo-post', dest='demoPost', action=argparse.BooleanOptionalAction, help='Make demo POST request')
+parser.add_argument('--demo-files', dest='demoFiles', action=argparse.BooleanOptionalAction, help='Open the app with links to demo files')
+
+parser.add_argument('--demo-files-data-folder', dest='demoFilesDataFolder', metavar='{demoFilesDataFolder}', action='store', default=defaultDataFolder, help='Data folder name (default: "' + defaultDataFolder + '")')
+parser.add_argument('--demo-files-data-set-folder', dest='demoFilesDataSetFolder', metavar='{demoFilesDataSetFolder}', action='store', default=defaultDataSetFolder, help='Data set folder name (default: "' + defaultDataSetFolder + '")')
+parser.add_argument('--demo-files-target-folder', dest='demoFilesTargetFolder', metavar='{demoFilesTargetFolder}', action='store', default=defaultTargetFolder, help='Target folder name (default: "' + defaultTargetFolder + '")')
+parser.add_argument('--demo-files-omit-date-tag', dest='demoFilesOmitDateTag', action=argparse.BooleanOptionalAction, help='Omit date tag postfix for auto-generated target folder name (datetime module required)')
+
+# \<\(dataFolder\|dataSetFolder\|targetFolder\|omitDateTag\)\>
+
 parser.add_argument('--dev', dest='isDev', action=argparse.BooleanOptionalAction, help='Use "public" folder prefix for demo data files and "' + devBuildFolder + '" for local web server (for non-built dev environment)')
+
 options = parser.parse_args()
 
 
@@ -98,15 +118,15 @@ def loadDemoAppData() -> AppData:
     """
 
     print('isDev:', options.isDev)
-    print('dataFolder:', options.dataFolder)
-    print('dataSetFolder:', options.dataSetFolder)
+    print('demoFilesDataFolder:', options.demoFilesDataFolder)
+    print('demoFilesDataSetFolder:', options.demoFilesDataSetFolder)
 
     dataPathParts = [
         'public' if options.isDev else None,  # For pre-build environment, when 'public' hasn't served at root of build files yet.
-        options.dataFolder,
+        options.demoFilesDataFolder,
     ]
     dataFolder = '/'.join(list(filter(None, dataPathParts)))
-    dataSetFolder = options.dataSetFolder
+    dataSetFolder = options.demoFilesDataSetFolder
 
     dataPath = posixPath(path.join(rootPath, dataFolder))
     dataSetPath = posixPath(path.join(dataPath, dataSetFolder))
@@ -132,8 +152,8 @@ def loadDemoAppData() -> AppData:
     return appData
 
 def getTargetFolder() -> str:
-    targetFolder = options.targetFolder
-    if not options.omitDateTag:
+    targetFolder = options.demoFilesTargetFolder
+    if not options.demoFilesOmitDateTag:
         targetFolder += '-' + getDateTag()
     return targetFolder
 
@@ -145,13 +165,15 @@ def ensureTargetFolder(targetFolder: str):
         # TODO: To catch possible fs/io errors?
         os.makedirs(targetPath)
 
-def getTargetFileNames(targetFolder: str) -> TargetFileNames:
+def getTargetFileNames(targetFolder: str, id: str = '') -> TargetFileNames:
+    prefix = (id + '-' if id else '');
+    postfix = '.json';
     # Create target file names...
     targetFileNames: TargetFileNames = {
-        'edges': posixPath(path.join(targetFolder, 'edges.json')),
-        'flows': posixPath(path.join(targetFolder, 'flows.json')),
-        'graphs': posixPath(path.join(targetFolder, 'graphs.json')),
-        'nodes': posixPath(path.join(targetFolder, 'nodes.json')),
+        'edges': posixPath(path.join(targetFolder, prefix + 'edges' + postfix)),
+        'flows': posixPath(path.join(targetFolder, prefix + 'flows' + postfix)),
+        'graphs': posixPath(path.join(targetFolder, prefix + 'graphs' + postfix)),
+        'nodes': posixPath(path.join(targetFolder, prefix + 'nodes' + postfix)),
     }
     return targetFileNames
 
@@ -202,15 +224,15 @@ def removeTempAppData(targetFolder: str,targetFileNames: TargetFileNames):
 
 # Web server...
 
-def getUrlQuery(targetFileNames: TargetFileNames):
+def getAppUrlQuery(targetFileNames: TargetFileNames):
     return '&'.join([
         'doAutoLoad=yes',
+        'doAutoStart=yes',
         'autoLoadUrlEdges=' + targetFileNames['edges'],
         'autoLoadUrlFlows=' + targetFileNames['flows'],
         'autoLoadUrlGraphs=' + targetFileNames['graphs'],
         'autoLoadUrlNodes=' + targetFileNames['nodes'],
     ])
-
 
 class WebServer(threading.Thread):
     def __init__(self):
@@ -220,63 +242,225 @@ class WebServer(threading.Thread):
         self.ws = HTTPServer((self.host, self.port), WebHandler)
 
     def run(self):
-        print('WebServer started at port:', self.port)
+        print('Web server has started at port:', self.port)
         self.ws.serve_forever()
 
     def shutdown(self):
         # Set the two flags needed to shutdown the HTTP server manually
         self.ws._BaseServer__is_shut_down.set()
         self.ws.__shutdown_request = True
-        print('Shutting down server')
+        print('Shutting down server...')
         # Call it anyway, for good measure...
         self.ws.shutdown()
-        print('Closing server')
+        print('Closing server...')
         self.ws.server_close()
-        print('Closing thread')
+        print('Closing thread...')
         self.join()
 
-class WebHandler(http.server.SimpleHTTPRequestHandler):
+class WebHandler(http.server.CGIHTTPRequestHandler):  # Instead of `SimpleHTTPRequestHandler` in order to serve post requests
+    # TODO: do_HEAD, do_POST
+    # @see:
+    # - https://docs.python.org/3/library/http.server.html
+    # - https://copyprogramming.com/howto/python-python-cgi-get-post-data-code-example
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, directory=webServerRootPath, **kwargs)
 
-def launchBrowser():
+    # Process post requests...
+    def do_POST(self):
+        global options
+        try:
+            url = self.path
+            if url != acceptPostDataUrl:
+                self.send_error(404, message='Route not found')
+                self.end_headers()
+                return
+            #  print('do_POST: start', {
+            #      'command': self.command,
+            #      'directory': self.directory,
+            #      'self': self,
+            #  })
+            # Headers...
+            headers = self.headers
+            headerKeys = headers.keys()
+            origin = headers.get('origin')
+            referer = headers.get('referer')
+            # Prepare unique file id...
+            fileId = ('post-' + referer)
+            fileId = re.sub(r'[^\w]+', ' ', fileId).strip();
+            fileId = re.sub(r'[ \s]+', '-', fileId)
+            if not options.demoFilesOmitDateTag:
+                fileId += '-' + getDateTag()
+            logPrefix = 'POST ' + acceptPostDataUrl + ':'
+            print(logPrefix, 'Starting to prepare data in folder "' + targetFolder + '" with data id: "' + fileId + '"')
+            dataLength = int(headers.get('content-length'))
+            # Form...
+            form = cgi.FieldStorage(
+                fp=self.rfile,
+                headers=headers,
+                environ={'REQUEST_METHOD': 'POST'}
+            )
+            formType = form.type
+            formKeys = form.keys()
+            # NOTE: Get data contents for data:
+            # - edges
+            # - flows
+            # - graphs
+            # - nodes
+            # ...and write it to data files...
+            edges = form.getvalue('edges')
+            flows = form.getvalue('flows')
+            graphs = form.getvalue('graphs')
+            nodes = form.getvalue('nodes')
+            # Check data existency...
+            if not edges or not flows or not graphs or not nodes:
+                raise Exception('One of four required data sets (edges, flows, graphs, nodes) hasn\'t been defined!')
+            # Prepare file names and app url query...
+            targetFileNames = getTargetFileNames(targetFolder, fileId)
+            urlQuery = getAppUrlQuery(targetFileNames)
+            # Create data storage...
+            try:
+                edgesData = json.loads(edges)
+            except Exception as error:
+                raise Exception('Edges json data parse error: ' + str(error))
+            try:
+                flowsData = json.loads(flows)
+            except Exception as error:
+                raise Exception('Flows json data parse error: ' + str(error))
+            try:
+                graphsData = json.loads(graphs)
+            except Exception as error:
+                raise Exception('Graphs json data parse error: ' + str(error))
+            try:
+                nodesData = json.loads(nodes)
+            except Exception as error:
+                raise Exception('Nodes json data parse error: ' + str(error))
+            appData: AppData = {
+                'edges': edgesData,
+                'flows': flowsData,
+                'graphs': graphsData,
+                'nodes': nodesData,
+            }
+            #  print('do_POST: got params', {
+            #      # Headers...
+            #      'headers': headers,
+            #      'headerKeys': headerKeys,
+            #      'dataLength': dataLength,
+            #      # Form...
+            #      'form': form,
+            #      'formType': formType,
+            #      'formKeys': formKeys,
+            #      # Data...
+            #      'edges': edges,
+            #      'flows': flows,
+            #      'graphs': graphs,
+            #      'nodes': nodes,
+            #      # Prepared data
+            #      'fileId': fileId,
+            #      'targetFileNames': targetFileNames,
+            #      'urlQuery': urlQuery,
+            #      'appData': appData,
+            #  })
+            # Write files...
+            ensureTargetFolder(targetFolder)
+            writeTempAppData(appData, targetFileNames)
+            # TODO: Set delayed 'garbage collector' here to remove unused files after some period of time?
+            # OK. Finish the response with redirect to the app page...
+            print(logPrefix, 'Data with id "' + fileId + '" has been successfully created, redirecting to the main app')
+            self.send_response(303)
+            self.send_header('Status', '303 Going to main app page')
+            self.send_header('Location', '/?' + urlQuery)
+            self.end_headers()
+            #  self.wfile.write('Response content'.encode())
+
+        except Exception as error:
+            sTraceback = str(traceback.format_exc())
+            sError = str(error)
+            # 'Server error occured. See server log for more details.'
+            self.send_error(400, message=sError, explain=sTraceback)
+            self.end_headers()
+
+
+# Web client...
+
+def launchBrowserWithFiles():
     """
     Launch browser and pass created file names in url query.
     """
-    urlQuery = getUrlQuery(targetFileNames)
+    global targetFileNames
+    urlQuery = getAppUrlQuery(targetFileNames)
     url = browserUrlPrefix + str(options.webPort) + '/?' + urlQuery
     print('Opening browser with url:', url)
     webbrowser.open(url, new=0, autoraise=True)
 
-def launchServer(targetFolder: str, targetFileNames: TargetFileNames):
-    """
-    Launch local web server to open app.
-    """
-    webServer = WebServer()
-    webServer.start()
-    launchBrowser()
-    # TODO: Start this keyboard listening loop in the background
+
+# Server helpers...
+
+def isDemoMode():
+    return options.demoPost or options.demoFiles
+
+def waitForKeyboardInterrrupt():
+    global targetFolder, demoTargetFileNames, webServer
     while True:
         try:
             sleep(1)
         except KeyboardInterrupt:
-            print('Keyboard Interrupt sent')
-            webServer.shutdown()
-            # Call cleanup code here
-            removeTempAppData(targetFolder,targetFileNames)
+            print('Keyboard interrupt has cought!')
+            print('Stopping the web server (don\'t forget to close the browser manually)...')
+            if webServer:
+                webServer.shutdown()
+                webServer = None
+            # Clean previously written files (if run demo mode)...
+            isDemo = isDemoMode()
+            if isDemo:
+                removeTempAppData(targetFolder, demoTargetFileNames)
             # TODO: Use callbacks?
             exit(0)
 
+def launchServer():
+    global targetFolder, webServer
+    """
+    Launch local web server to open app in background.
+    """
+    print('Starting the web server')
+    webServer = WebServer()
+    webServer.start()
 
-# Start processing...
+
+# Set global variables...
 
 targetFolder = getTargetFolder()
-targetFileNames = getTargetFileNames(targetFolder)
+demoTargetFileNames = getTargetFileNames(targetFolder, 'demo')
 
-# DEBUG: Get demo data from files. Provide real data here.
-appData = loadDemoAppData()
+webServer = None
 
-ensureTargetFolder(targetFolder)
-writeTempAppData(appData, targetFileNames)
 
-launchServer(targetFolder, targetFileNames)
+# Start...
+
+isDemo = isDemoMode()
+
+# Prepare demo data (if in demo mode)...
+if isDemo:
+    # DEBUG: Get demo data from files. It's possible to provide real data here.
+    appData = loadDemoAppData()
+
+    ensureTargetFolder(targetFolder)
+    writeTempAppData(appData, demoTargetFileNames)
+
+# Start web server (in a separate thread)...
+
+serverThread = threading.Thread(target=launchServer)
+serverThread.start()
+
+# TODO: If passed parameter (`do-demo-post`) make a post request with a demo data
+if options.demoPost:
+    print('Starting demo post here...')
+    # TODO: Create an intermediate page which should read the prepared files and then make a POST request with that data
+    #  launchBrowserWithPostRequestPage()
+elif options.demoFiles:
+    launchBrowserWithFiles()
+elif isDemo:
+    removeTempAppData(targetFolder, demoTargetFileNames)
+
+# Waiting for Ctrl-C to finish the web server...
+waitForKeyboardInterrrupt()
