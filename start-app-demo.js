768bb64e320565d63a9cbe4c40cc1047c12c0aa1
lilliputten <lilliputten@yandex.ru>
2024-02-03 23:25:03 +0700

    Issue #29: Updated unique file name to include origin ip address. Modifed post reponse to return created url to redirect. Created optional parameter to behave in the old way: to make redirect immediatelly (?redirect=[non-null]). Restored removed demo scripts (from git history). Made a few more demos to show & test new ways to open app.

---
+++ start-app-demo.js
@@ -0,0 +1,131 @@
+/* eslint-disable no-console */
+/**
+ * @changed 2024.02.03, 21:15
+ */
+
+const postRequestUrl = '/cgi-bin/accept-post-data?redirect=false';
+
+let receivedUrl;
+
+function onLoad() {
+  console.log('[start-app-demo-watch:onLoad]');
+}
+
+function sendPostDataRequest() {
+  const edges = document.getElementById('edges')?.value;
+  const flows = document.getElementById('flows')?.value;
+  const graphs = document.getElementById('graphs')?.value;
+  const nodes = document.getElementById('nodes')?.value;
+  const requestData = {
+    edges,
+    flows,
+    graphs,
+    nodes,
+  };
+  const fetchParams = {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    // redirect: 'follow', // manual, *follow, error
+    // referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(requestData), // body data type must match "Content-Type" header
+  };
+  const resultNode = document.getElementById('result');
+  const resultWrapperNode = document.getElementById('resultWrapper');
+  console.log('[start-app-demo-watch:sendPostDataRequest] start', {
+    resultNode,
+    postRequestUrl,
+    fetchParams,
+  });
+  fetch(postRequestUrl, fetchParams)
+    .then((res) => {
+      const { ok, body } = res;
+      if (!ok) {
+        return res.text().then((text) => {
+          throw new Error(text);
+        });
+      }
+      console.log('[start-app-demo-watch:sendPostDataRequest] response', {
+        ok,
+        body,
+        res,
+      });
+      return res.text();
+    })
+    .then((text) => {
+      let data;
+      try {
+        data = JSON.parse(text);
+      } catch (err) {
+        console.warn('Cannot parse json text', { text });
+      }
+      let result = text;
+      receivedUrl = undefined;
+      if (data) {
+        receivedUrl = data.url;
+        // prettier-ignore
+        result = '\n'
+          + '<h4>Data:</h4>\n'
+          + '<pre>' + JSON.stringify(data, null, 2) + '</pre>\n'
+          + '<h4>Text:</h4>\n'
+          + '<div>' + text + '</div>\n'
+        ;
+        if (receivedUrl) {
+          // prettier-ignore
+          result = '\n'
+            + '<h4>Received url:</h4>\n'
+            + '<div>' + receivedUrl + '</div>\n'
+        }
+      }
+      console.log('[start-app-demo-watch:sendPostDataRequest] result', {
+        receivedUrl,
+        result,
+        text,
+        data,
+      });
+      resultNode.innerHTML = result;
+      resultWrapperNode.classList.toggle('error', false);
+      document.getElementById('goToReceivedUrl')?.classList.toggle('hidden', !receivedUrl);
+    })
+    .catch((err) => {
+      let errText = err.message || String(err);
+      const isHtml = /<html/i.test(errText);
+      if (isHtml) {
+        errText = errText
+          .replace(/<head>[\s\S]*<\/head>/i, '')
+          .replace(/<[^<>]*>/g, '')
+          // .replace(/[ \t]+/g, ' ')
+          // .replace(/\n\s+/g, '\n')
+          .replace(/\s+\n/g, '\n')
+          .trim();
+      }
+      console.error('[start-app-demo-watch:sendPostDataRequest] error', {
+        errText,
+        isHtml,
+        err,
+      });
+      // eslint-disable-next-line no-debugger
+      debugger;
+      resultNode.innerText = errText;
+      resultWrapperNode.classList.toggle('error', true);
+    })
+    .finally(() => {
+      resultWrapperNode.classList.toggle('hidden', false);
+    });
+}
+
+function goToReceivedUrl() {
+  console.log('[start-app-demo-watch:goToReceivedUrl]', {
+    receivedUrl,
+  });
+  window.location.href = receivedUrl;
+}
+
+window.sendPostDataRequest = sendPostDataRequest;
+window.goToReceivedUrl = goToReceivedUrl;
+
+window.addEventListener('load', onLoad);
