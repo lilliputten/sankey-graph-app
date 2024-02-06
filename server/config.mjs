60b7272200396c8cd6b470fa071de817696eb767
lilliputten <lilliputten@yandex.ru>
2024-02-06 13:07:10 +0700

    Issue #31: Added processing of static routes.

--- server/config.mjs
+++ server/config.mjs
@@ -1,29 +1,59 @@
 // @ts-check
 
+import path from 'path';
 import ip from 'ip';
+import { fileURLToPath } from 'url';
+
+// Local environment...
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const currPath = path.resolve(__dirname);
+export const rootPath = path.dirname(path.basename(currPath));
+
+// Network environment...
+
+export const realIPAddress = ip.address();
 
 // Constants...
+
 export const browserUrlPrefix = 'http://localhost:';
 export const devBuildFolder = 'build';
 
 export const acceptPostDataUrl = '/cgi-bin/accept-post-data';
 
 // Default options...
+
 export const defaultDataFolder = 'data';
 export const defaultDataSetFolder = 'hardwood-forestry';
 export const defaultTargetFolder = 'temp';
 export const defaultWebPort = 8080;
 
 // Helper's constants...
+
 export const ipv6remove = '::ffff:';
 export const ipv6localhost = '::1';
 // export const localhostAddress = 'localhost';
-
-// Environment...
-export const realIPAddress = ip.address();
 
 /* TODO: Potential options...
  * uploadDir
  * replaceUploadsDir
  * useDebugStops
  */
+
+// Server options...
+
+export const maxAgeShort = 1000 * 60 * 1; // *ms = 1m
+export const maxAgeLong = maxAgeShort * 60; // = 1h
+export const maxAgeStatic = maxAgeLong; // maxAge value (ms) for caching static routes
+
+// Express parameters...
+
+export const staticServerRoutes = [
+  // TODO: Invlove `dev` mode?
+  'start-app-demo-post.html',
+  'start-app-demo.js',
+  'build.txt', // NOTE: Only for built app
+  'favicon.ico',
+];
