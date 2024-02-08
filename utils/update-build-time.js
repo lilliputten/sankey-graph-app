54fbcd98b5d5828582a0da2a34de079fe23eaa4a
lilliputten <lilliputten@yandex.ru>
2024-02-08 17:51:06 +0700

    Issue #34: Updated date usages in `update-build-time.js`. Updated version: 0.0.27.

--- utils/update-build-time.js
+++ utils/update-build-time.js
@@ -1,11 +1,18 @@
 /** @desc Update build date/time tag file with current timestamp
- *  @changed 2023.04.07, 23:39
+ *  @changed 2024.02.08, 17:50
  */
 /* eslint-disable no-console */
 
 const fs = require('fs');
 const path = require('path');
-const { format, formatInTimeZone } = require('date-fns-tz');
+
+const dayjsUtc = require('dayjs/plugin/utc.js');
+const dayjsTimezone = require('dayjs-timezone-iana-plugin'); // @see https://day.js.org/docs/en/plugin/timezone
+const dayjs = require('dayjs'); // @see https://day.js.org/docs/en/display/format
+
+dayjs.extend(dayjsUtc);
+dayjs.extend(dayjsTimezone);
+
 
 const currPath = path.resolve(__dirname);
 const prjPath = path.dirname(path.basename(currPath));
@@ -15,16 +22,19 @@ const config = require(configFileName);
 
 const defaultTimeZome = config.timeZone || '';
 
-const tagFormat = 'yyMMdd-HHmm';
-const timeFormat = 'yyyy.MM.dd, HH:mm';
-const timeTzFormat = 'yyyy.MM.dd, HH:mm zzz';
+// NOTE: Date formats for 'dayjs', @see https://day.js.org/docs/en/display/format
+const tagFormat = 'YYMMDD-HHmm';
+const timeTzFormat = 'YYYY.MM.DD HH:mm ZZ';
+/** // NOTE: Date formats for `date-fns*` library...
+ * const tagFormat = 'yyMMdd-HHmm';
+ * const timeTzFormat = 'yyyy.MM.dd, HH:mm zzz';
+ */
 
 // Timezone argument should be passed as '--tz={timeZone}`
 const timeZone = getTzFromArgs() || defaultTimeZome; // 'Europe/Moscow', 'GMT, etc
 
 const now = new Date();
 const buildTag = formatDate(now, timeZone, tagFormat);
-const buildTime = formatDate(now, timeZone, timeFormat);
 const buildTzTime = formatDate(now, timeZone, timeTzFormat);
 
 /* console.log('DEBUG', {
@@ -50,11 +60,19 @@ fs.writeFileSync(timetagFileName, buildTag, 'utf8');
 fs.writeFileSync(timestampFileName, buildTzTime, 'utf8');
 
 function formatDate(date, timeZone, fmt) {
+  let dayjsDate = dayjs(date);
+  if (timeZone) {
+    dayjsDate = dayjsDate.tz(timeZone);
+  }
+  const fmtDate = dayjsDate.format(fmt);
+  return fmtDate;
+  /* // OLD_CODE: Using 'date-fns-tz'
   if (timeZone) {
     return formatInTimeZone(date, timeZone, fmt);
   } else {
     return format(date, fmt);
   }
+  */
 }
 
 function getTzFromArgs() {
