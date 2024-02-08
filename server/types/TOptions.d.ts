ad5f3c81fbea964e7bd05d52840b8a47fcb2792c
lilliputten <lilliputten@yandex.ru>
2024-02-08 18:48:27 +0700

    Issue #33: Updated readme's and app help content to meet the usage of web servers (python and node). Fixed minor code issues.

--- server/types/TOptions.d.ts
+++ server/types/TOptions.d.ts
@@ -1,16 +1,17 @@
 interface TOptions {
   'web-port': string | number;
 
-  'demo-post': boolean;
-  'demo-files': boolean;
-
   'files-target-folder': string;
   'files-omit-date-tag': string;
 
   dev: boolean;
 
-  'demo-files-data-folder': string;
-  'demo-files-data-set-folder': string;
-
   help: boolean;
+
+  /* // NOTE: These demo options hasn't been implemented in js server. It's possible to use `start-app-demo-post.html` demo page (it's already included in the `build` folder).
+   * 'demo-post': boolean;
+   * 'demo-files': boolean;
+   * 'demo-files-data-folder': string;
+   * 'demo-files-data-set-folder': string;
+   */
 }
--- src/components/SankeyMisc/HelpContent/HelpContent.tsx
+++ src/components/SankeyMisc/HelpContent/HelpContent.tsx
@@ -215,7 +215,7 @@ $python start-app.py --help
 
 Launch web server for the app.
 
-**options:**
+**Options:**
 
 **-h, --help**
 show this help message and exit
@@ -245,6 +245,85 @@ module required)
 **--dev**
 Use "public" folder prefix for demo data files and "build" for local web
 server (for non-built dev environment)
+
+
+## Run application with nodejs server
+
+It's possible to run the app with nodejs server (it doesn't have freezes)
+
+\`\`\`
+node start-server.mjs
+\`\`\`
+
+The help info is also available:
+
+$ node start-server.mjs --help
+
+**Description**
+
+Launch web server for the app.
+
+**Options**
+
+**--web-port, -p**
+Web server port (default: "8080")
+
+**--files-target-folder**
+Target folder name (default: "temp")
+
+**--files-omit-date-tag**
+Omit date tag postfix for auto-generated target folder name (datetime module required)
+
+**--dev, -d**
+Use "build" prefix for local web server (for non- built dev environment)
+
+**--help, -h**
+Print this help and quit
+
+Some demo options (\`demo-post\`, \`demo-files\`, \`demo-files-data-folder\`, \`demo-files-data-set-folder\`) hasn't been implemented in js server. But it's possible to use \`start-app-demo-post.html\` demo page (it's already included in the \`build\` folder).
+
+
+## Transfer data to serverand open the app with it
+
+Both servers allow to send Sankey data to them and open an application with this transferred data.
+
+The endpoint address to send data is \`POST /cgi-bin/accept-post-data\` (this particular name was choosen due to python embedded http server limitations for \`CGIHTTPRequestHandler\` POST requests).
+
+It accpets both json (\`application/json\`) and multipart form (\`application/x-www-form-urlencoded\`) formats. Thus, it can be sent using an ajax request or published from a standard html form.
+
+The expected data format should contain four fields: \`edges\`, \`flows\`, \`graphs\`, \`nodes\` (respectively for standard sankey files: \`edges.json\`, \`flows.json\`, \`nodes-supply-chain.json\`, \`nodes.json\`).
+
+Those fields can be either data (for json-formatted objects) or a json-encoded string (for forms). The data is saved to a temporary folder (the name of which comes from the \`files-target-folder\` and \`files-omit-date-tag\` arguments).
+
+Then (in the case of success) there will be generated =a link like this one (without any spaces):
+
+\`\`\`
+<WEB-SERVER-ADDRESS>
+?doAutoLoad=yes
+&doAutoStart=yes
+&autoLoadUrlEdges=temp-240202-220842/post-http-localhost-8080-start-app-demo-post-html-240202-220848-edges.json
+&autoLoadUrlFlows=temp-240202-220842/post-http-localhost-8080-start-app-demo-post-html-240202-220848-flows.json
+&autoLoadUrlGraphs=temp-240202-220842/post-http-localhost-8080-start-app-demo-post-html-240202-220848-graphs.json
+&autoLoadUrlNodes=temp-240202-220842/post-http-localhost-8080-start-app-demo-post-html-240202-220848-nodes.json
+\`\`\`
+
+By default this link will be returned in the application/json response object with a single field \`url\` (\`{ url: <URL-VALUE> }\`).
+
+If there was passed a 'truthy' url query parameter \`redirect\` (eg, \`<...>//cgi-bin/accept-post-data?redirect=yes\`), then the immediate redircit to this link will be occured (it'll be useful in a case of regular form submit).
+
+
+## Open a data post demo page:
+
+1. Start web server with: \`node start-server.mjs\` (or \`python start-app.py\`; if starting form the project folder, with \`build\` subfolder, then use \`--dev\` comman line key).
+2. Open demo html page in a browser: \`http://localhost:8080/start-app-demo-post.html\`.
+3. Submit a form manually (with a 'Submit form...' button) or send ajax request (with a 'Send post request...' button). It'll be sent to data processing endpoint \`POST /cgi-bin/accept-post-data\`.
+4. When/if data is successfully created it'll be redirected to main app page with corresponding parameters in the url query, or the url will be returned in response (then the highlighted button 'Follow received url' will appear and the url will be shown bellow).
+
+File names here formed with request headers' origin string and current timestamp to prevent reuse the same names for different clients.
+
+Remember to close the browser or release the page in order to stop the server.
+
+If you want to made changes in server files (in a 'dev' mode), it's possible to start a dynamic updater with \`sh start-app-demo-watch.sh\`.
 
 `;
 }
